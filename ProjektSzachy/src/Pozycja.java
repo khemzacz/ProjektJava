8bf5f68cb98bcc8a6243df447891b0cb671cec13
Konrad <Konrad@Mobilek.local>
2015-04-12 16:17:30 +0200

    śnię ja javie

---
+++ ProjektSzachy/src/Pozycja.java
@@ -0,0 +1,13 @@
+
+public class Pozycja {
+	public int row;
+	public int column;
+	
+	Pozycja (int row, int column)
+	{
+		this.row = row;
+		this.column = column;		
+	}
+	
+	
+}
--- ProjektSzachy/src/SzachyLogika.java
+++ ProjektSzachy/src/SzachyLogika.java
@@ -1,26 +1,69 @@
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class SzachyLogika {
 	private static String plansza[][];
-	public enum figury {PUSTE,PION, WIEZA, KON, GONIEC, HETMAN, KROL}
+	public enum bierki {PUSTE,PION, WIEZA, KON, GONIEC, HETMAN, KROL}
 	
-	void SzachyLogika()
+	
+	
+	public void SzachyLogika()
 	{
 		resetujplansze();
 	}
 	
-	void resetujplansze()
+	public void resetujplansze()
 	{
 		plansza = new String[][] {	 
-					 {"w","s","g","k","h","g","s","w"},
+					 {"w","s","g","h","k","g","s","w"},
 					 {"p","p","p","p","p","p","p","p"},	
 					 {" "," "," "," "," "," "," "," "},
 					 {" "," "," "," "," "," "," "," "},
 					 {" "," "," "," "," "," "," "," "},
 					 {" "," "," "," "," "," "," "," "},
 					 {"P","P","P","P","P","P","P","P"},
-					 {"W","S","G","K","H","G","S","W"}};
-		
+					 {"W","S","G","h","k","G","S","W"}};
 	}
 	
+	public List<Pozycja> possibleMoves(String plansza[][],Pozycja pos )
+	{
+		//String pMoves[];
+		List<Pozycja> pMoves = new ArrayList<Pozycja>();
+		switch (plansza[pos.row][pos.column])
+		{
+			case "p":
+			{
+				if (pos.row == 6)
+				{
+					if(plansza[pos.row+1][pos.column].equals(" "))
+					{
+						pMoves.add(new Pozycja(pos.row+1,pos.column));
+						if(plansza[pos.row+2][pos.column].equals(" "))
+						{
+							pMoves.add(new Pozycja(pos.row+2,pos.column));
+						}
+					}
+				}
+				
+			}
+			case "P":	
+			{
+				
+			}	
+		
+		}
+		
+		
+		
+		
+		return pMoves;
+	}
+	
+	public void zbij(String plansza[][], int row, int column)
+	{
+		
+		
+	}
 	
 }
