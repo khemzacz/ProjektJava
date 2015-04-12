68b168e4175d0193217267ca96f38e308cd2853d
Konrad <Konrad@Mobilek.local>
2015-04-12 17:43:22 +0200

    C++ Był Lepszy

---
+++ ProjektSzachy/src/MojCharacter.java
@@ -0,0 +1,39 @@
+
+public class MojCharacter
+{
+	public Character c;
+	private Character duzeLitery[]= {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','R','S','T','U','W','X','Y','Z'};
+	private Character maleLitery[]= {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u','w','x','y','z'};
+	public boolean isUpperCase()
+	{
+		for (int i =0 ;i<24;i++)
+		{
+			if (c.equals(duzeLitery[i]))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean isLowerCase()
+	{
+		for(int i =0;i<24;i++)
+		{
+			if(c.equals(maleLitery[i]))
+			{
+				return true;
+			}
+			
+		}
+		return false;
+	}
+	
+	MojCharacter(Character c)
+	{
+		this.c=c;
+	}
+	
+	void operator (Character c)
+	{this.c=c;}
+}
--- ProjektSzachy/src/SzachyLogika.java
+++ ProjektSzachy/src/SzachyLogika.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 
 public class SzachyLogika {
-	private static String plansza[][];
+	private static MojCharacter plansza[][];
 	public enum bierki {PUSTE,PION, WIEZA, KON, GONIEC, HETMAN, KROL}
 	
 	
@@ -15,39 +15,43 @@ public class SzachyLogika {
 	
 	public void resetujplansze()
 	{
-		plansza = new String[][] {	 
-					 {"w","s","g","h","k","g","s","w"},
-					 {"p","p","p","p","p","p","p","p"},	
-					 {" "," "," "," "," "," "," "," "},
-					 {" "," "," "," "," "," "," "," "},
-					 {" "," "," "," "," "," "," "," "},
-					 {" "," "," "," "," "," "," "," "},
-					 {"P","P","P","P","P","P","P","P"},
-					 {"W","S","G","h","k","G","S","W"}};
+		plansza = new MojCharacter[][] {	 
+				 {new MojCharacter('w'),new MojCharacter('s'),new MojCharacter('g'),new MojCharacter('h'),new MojCharacter('k'),new MojCharacter('g'),new MojCharacter('s'),new MojCharacter('w')},
+				 {new MojCharacter('p'),new MojCharacter('p'),new MojCharacter('p'),new MojCharacter('p'),new MojCharacter('p'),new MojCharacter('p'),new MojCharacter('p'),new MojCharacter('p')},	
+				 {new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' ')},
+				 {new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' ')},
+				 {new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' ')},
+				 {new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' ')},
+				 {new MojCharacter('P'),new MojCharacter('P'),new MojCharacter('P'),new MojCharacter('P'),new MojCharacter('P'),new MojCharacter('P'),new MojCharacter('P'),new MojCharacter('P')},
+				 {new MojCharacter('W'),new MojCharacter('S'),new MojCharacter('G'),new MojCharacter('h'),new MojCharacter('k'),new MojCharacter('G'),new MojCharacter('S'),new MojCharacter('W')}};
 	}
 	
-	public List<Pozycja> possibleMoves(String plansza[][],Pozycja pos )
+	public List<Pozycja> possibleMoves(Character plansza[][],Pozycja pos )
 	{
 		//String pMoves[];
 		List<Pozycja> pMoves = new ArrayList<Pozycja>();
 		switch (plansza[pos.row][pos.column])
 		{
-			case "p":
+			case 'p':
 			{
 				if (pos.row == 6)
 				{
-					if(plansza[pos.row+1][pos.column].equals(" "))
+					if(plansza[pos.row+1][pos.column].equals(' '))
 					{
 						pMoves.add(new Pozycja(pos.row+1,pos.column));
-						if(plansza[pos.row+2][pos.column].equals(" "))
+						if(plansza[pos.row+2][pos.column].equals(' '))
 						{
 							pMoves.add(new Pozycja(pos.row+2,pos.column));
 						}
 					}
+					if(plansza[pos.row-1][pos.column+1] == plansza[pos.row-1][pos.column+1].)
+					{
+						
+					}
 				}
 				
 			}
-			case "P":	
+			case 'P':	
 			{
 				
 			}	
