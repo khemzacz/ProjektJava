d73b80387bc037c4d3c78bcf03978fb7ccd5519f
Konrad <Konrad@Mobilek>
2015-04-21 15:41:08 +0200

    ruchy Hetmana i Gonca

--- ProjektSzachy/src/SzachyLogika.java
+++ ProjektSzachy/src/SzachyLogika.java
@@ -16,10 +16,10 @@ public class SzachyLogika {
 		plansza = new MojCharacter[][] {	 
 				 {new MojCharacter('w'),new MojCharacter('s'),new MojCharacter('g'),new MojCharacter('h'),new MojCharacter('k'),new MojCharacter('g'),new MojCharacter('s'),new MojCharacter('w')},
 				 {new MojCharacter('p'),new MojCharacter('p'),new MojCharacter('p'),new MojCharacter('p'),new MojCharacter('p'),new MojCharacter('p'),new MojCharacter('p'),new MojCharacter(' ')},	
+				 {new MojCharacter(' '),new MojCharacter(' '),new MojCharacter('G'),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' ')},
+				 {new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter('h'),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' ')},
+				 {new MojCharacter(' '),new MojCharacter(' '),new MojCharacter('H'),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' ')},
 				 {new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' ')},
-				 {new MojCharacter('p'),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' ')},
-				 {new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' ')},
-				 {new MojCharacter('w'),new MojCharacter('p'),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' '),new MojCharacter(' ')},
 				 {new MojCharacter('P'),new MojCharacter('P'),new MojCharacter('P'),new MojCharacter('P'),new MojCharacter('P'),new MojCharacter('P'),new MojCharacter('P'),new MojCharacter('P')},
 				 {new MojCharacter('W'),new MojCharacter('S'),new MojCharacter('G'),new MojCharacter('H'),new MojCharacter('K'),new MojCharacter('G'),new MojCharacter('S'),new MojCharacter('W')}};
 	}
@@ -46,12 +46,12 @@ public class SzachyLogika {
 		if (gra.plansza[pos.row][pos.column].rowne('g') || gra.plansza[pos.row][pos.column].rowne('G') )
 		{
 			System.out.println("Wykryto Gonca");
-			// return this.ruchyGonca(pos,gra);
+			return this.ruchyGonca(pos,gra);
 		}
 		if (gra.plansza[pos.row][pos.column].rowne('h') || gra.plansza[pos.row][pos.column].rowne('H') )
 		{
 			System.out.println("Wykryto Hetmana");
-			// return this.ruchyHetmana(pos,gra);			
+			return this.ruchyHetmana(pos,gra);			
 		}
 		if (gra.plansza[pos.row][pos.column].rowne('k') || gra.plansza[pos.row][pos.column].rowne('K') )
 		{
@@ -136,7 +136,7 @@ public class SzachyLogika {
 					break;
 				}
 			}
-			for (int i = pos.column;i<=7;i++)	//ruch w prawo
+			for (int i = pos.column+1;i<=7;i++)	//ruch w prawo
 			{
 				if(gra.plansza[pos.row][i].rowne(' '))
 				{
@@ -189,9 +189,6 @@ public class SzachyLogika {
 			}
 			
 			return pMoves;
-
-			
-			
 		}
 		if(gra.plansza[pos.row][pos.column].rowne('W'))
 		{
@@ -212,7 +209,7 @@ public class SzachyLogika {
 					break;
 				}
 			}
-			for (int i = pos.column;i<=7;i++)	//ruch w prawo
+			for (int i = pos.column+1;i<=7;i++)	//ruch w prawo
 			{
 				if(gra.plansza[pos.row][i].rowne(' '))
 				{
@@ -510,6 +507,552 @@ public class SzachyLogika {
 		return pMoves;
 	}
 	
+	
+	public List <Pozycja> ruchyGonca(Pozycja pos, SzachyLogika gra)
+	{
+		List <Pozycja> pMoves = new ArrayList<Pozycja>();
+		System.out.println("wiersz" + pos.row); System.out.println("kolumna" +pos.column);
+		if(gra.plansza[pos.row][pos.column].rowne('g'))
+		{
+			//System.out.println("tutaj");
+			for (int i=1;i<=7;i++)
+			{
+				
+				if((pos.row+i)<=7 && (pos.column+i) <=7)
+				{
+					//System.out.println(pos.row + " "+ pos.column); 3 1
+					if(gra.plansza[pos.row+i][pos.column+i].rowne(' '))
+					{
+						//System.out.println("tutaj"); // nie wchodzi
+						pMoves.add(new Pozycja(pos.row+i,pos.column+i));
+						continue;
+					}
+					if(gra.plansza[pos.row+i][pos.column+i].isUpperCase())
+					{
+						pMoves.add(new Pozycja(pos.row+i,pos.column+i));
+						break;
+					}
+					if(gra.plansza[pos.row+i][pos.column+i].isLowerCase())
+					{
+						break;
+					}					
+				}
+				
+				else
+					{break;}
+			}
+			
+			for (int i =1;i<=7;i++)
+			{
+				if(pos.row-i>=0 && pos.column-i >=0)
+				{
+					if(gra.plansza[pos.row-i][pos.column-i].rowne(' '))
+					{
+						pMoves.add(new Pozycja(pos.row-i,pos.column-i));
+						continue;
+					}
+					if(gra.plansza[pos.row-i][pos.column-i].isUpperCase())
+					{
+						pMoves.add(new Pozycja(pos.row-i,pos.column-i));
+						break;
+					}
+					if(gra.plansza[pos.row-i][pos.column-i].isLowerCase())
+					{
+						break;
+					}					
+				}
+				else break;				
+				
+			}
+			
+			for (int i =1;i<=7;i++)
+			{
+				if(pos.row-i>=0 && pos.column+i <=7)
+				{
+					if(gra.plansza[pos.row-i][pos.column+i].rowne(' '))
+					{
+						pMoves.add(new Pozycja(pos.row-i,pos.column+i));
+						continue;
+					}
+					if(gra.plansza[pos.row-i][pos.column+i].isUpperCase())
+					{
+						pMoves.add(new Pozycja(pos.row-i,pos.column+i));
+						break;
+					}
+					if(gra.plansza[pos.row-i][pos.column+i].isLowerCase())
+					{
+						break;
+					}					
+				}
+				else break;					
+			}
+			
+			for (int i =1;i<=7;i++)
+			{
+				if(pos.row+i<=7 && pos.column-i >=0)
+				{
+					if(gra.plansza[pos.row+i][pos.column-i].rowne(' '))
+					{
+						pMoves.add(new Pozycja(pos.row+i,pos.column-i));
+						continue;
+					}
+					if(gra.plansza[pos.row+i][pos.column-i].isUpperCase())
+					{
+						pMoves.add(new Pozycja(pos.row+i,pos.column-i));
+						break;
+					}
+					if(gra.plansza[pos.row+i][pos.column-i].isLowerCase())
+					{
+						break;
+					}					
+				}
+				else break;					
+			}			
+			
+			
+		}
+		if(gra.plansza[pos.row][pos.column].rowne('G'))
+		{
+			for (int i=1;i<=7;i++)
+			{
+				if(pos.row+i <= 7 && pos.column+i <= 7)
+				{
+					if(gra.plansza[pos.row+i][pos.column+i].rowne(' '))
+					{
+						pMoves.add(new Pozycja(pos.row+i,pos.column+i));
+						continue;
+					}
+					if(gra.plansza[pos.row+i][pos.column+i].isLowerCase())
+					{
+						pMoves.add(new Pozycja(pos.row+i,pos.column+i));
+						break;
+					}
+					if(gra.plansza[pos.row+i][pos.column+i].isUpperCase())
+					{
+						break;
+					}					
+				}
+				else break;
+			}
+			
+			for (int i =1;i<=7;i++)
+			{
+				if(pos.row-i >=0 && pos.column-i >= 0)
+				{
+					if(gra.plansza[pos.row-i][pos.column-i].rowne(' '))
+					{
+						pMoves.add(new Pozycja(pos.row-i,pos.column-i));
+						continue;
+					}
+					if(gra.plansza[pos.row-i][pos.column-i].isLowerCase())
+					{
+						pMoves.add(new Pozycja(pos.row-i,pos.column-i));
+						break;
+					}
+					if(gra.plansza[pos.row-i][pos.column-i].isUpperCase())
+					{
+						break;
+					}					
+				}
+				else break;				
+				
+			}
+			
+			for (int i =1;i<=7;i++)
+			{
+				if(pos.row-i >= 0 && pos.column+i <= 7)
+				{
+					if(gra.plansza[pos.row-i][pos.column+i].rowne(' '))
+					{
+						pMoves.add(new Pozycja(pos.row-i,pos.column+i));
+						continue;
+					}
+					if(gra.plansza[pos.row-i][pos.column+i].isLowerCase())
+					{
+						pMoves.add(new Pozycja(pos.row-i,pos.column+i));
+						break;
+					}
+					if(gra.plansza[pos.row-i][pos.column+i].isUpperCase())
+					{
+						break;
+					}					
+				}
+				else break;					
+			}
+			
+			for (int i =1;i<=7;i++)
+			{
+				if(pos.row+i <= 7 && pos.column-i >= 0)
+				{
+					if(gra.plansza[pos.row+i][pos.column-i].rowne(' '))
+					{
+						pMoves.add(new Pozycja(pos.row+i,pos.column-i));
+						continue;
+					}
+					if(gra.plansza[pos.row+i][pos.column-i].isLowerCase())
+					{
+						pMoves.add(new Pozycja(pos.row+i,pos.column-i));
+						break;
+					}
+					if(gra.plansza[pos.row+i][pos.column-i].isUpperCase())
+					{
+						break;
+					}					
+				}
+				else break;					
+			}			
+		}	
+		
+		
+		return pMoves;
+	}
+	
+	public List <Pozycja> ruchyHetmana(Pozycja pos, SzachyLogika gra)
+	{
+		List <Pozycja> pMoves = new ArrayList<Pozycja>();
+		if(gra.plansza[pos.row][pos.column].rowne('h'))
+		{
+			//System.out.println("tutaj");
+			for (int i=1;i<=7;i++)
+			{
+				
+				if((pos.row+i)<=7 && (pos.column+i) <=7)
+				{
+					//System.out.println(pos.row + " "+ pos.column); 3 1
+					if(gra.plansza[pos.row+i][pos.column+i].rowne(' '))
+					{
+						//System.out.println("tutaj"); // nie wchodzi
+						pMoves.add(new Pozycja(pos.row+i,pos.column+i));
+						continue;
+					}
+					if(gra.plansza[pos.row+i][pos.column+i].isUpperCase())
+					{
+						pMoves.add(new Pozycja(pos.row+i,pos.column+i));
+						break;
+					}
+					if(gra.plansza[pos.row+i][pos.column+i].isLowerCase())
+					{
+						break;
+					}					
+				}
+				
+				else
+					{break;}
+			}
+			
+			for (int i =1;i<=7;i++)
+			{
+				if(pos.row-i>=0 && pos.column-i >=0)
+				{
+					if(gra.plansza[pos.row-i][pos.column-i].rowne(' '))
+					{
+						pMoves.add(new Pozycja(pos.row-i,pos.column-i));
+						continue;
+					}
+					if(gra.plansza[pos.row-i][pos.column-i].isUpperCase())
+					{
+						pMoves.add(new Pozycja(pos.row-i,pos.column-i));
+						break;
+					}
+					if(gra.plansza[pos.row-i][pos.column-i].isLowerCase())
+					{
+						break;
+					}					
+				}
+				else break;				
+				
+			}
+			
+			for (int i =1;i<=7;i++)
+			{
+				if(pos.row-i>=0 && pos.column+i <=7)
+				{
+					if(gra.plansza[pos.row-i][pos.column+i].rowne(' '))
+					{
+						pMoves.add(new Pozycja(pos.row-i,pos.column+i));
+						continue;
+					}
+					if(gra.plansza[pos.row-i][pos.column+i].isUpperCase())
+					{
+						pMoves.add(new Pozycja(pos.row-i,pos.column+i));
+						break;
+					}
+					if(gra.plansza[pos.row-i][pos.column+i].isLowerCase())
+					{
+						break;
+					}					
+				}
+				else break;					
+			}
+			
+			for (int i =1;i<=7;i++)
+			{
+				if(pos.row+i<=7 && pos.column-i >=0)
+				{
+					if(gra.plansza[pos.row+i][pos.column-i].rowne(' '))
+					{
+						pMoves.add(new Pozycja(pos.row+i,pos.column-i));
+						continue;
+					}
+					if(gra.plansza[pos.row+i][pos.column-i].isUpperCase())
+					{
+						pMoves.add(new Pozycja(pos.row+i,pos.column-i));
+						break;
+					}
+					if(gra.plansza[pos.row+i][pos.column-i].isLowerCase())
+					{
+						break;
+					}					
+				}
+				else break;					
+			}			
+			
+			
+		}
+		if(gra.plansza[pos.row][pos.column].rowne('H'))
+		{
+			for (int i=1;i<=7;i++)
+			{
+				if(pos.row+i <= 7 && pos.column+i <= 7)
+				{
+					if(gra.plansza[pos.row+i][pos.column+i].rowne(' '))
+					{
+						pMoves.add(new Pozycja(pos.row+i,pos.column+i));
+						continue;
+					}
+					if(gra.plansza[pos.row+i][pos.column+i].isLowerCase())
+					{
+						pMoves.add(new Pozycja(pos.row+i,pos.column+i));
+						break;
+					}
+					if(gra.plansza[pos.row+i][pos.column+i].isUpperCase())
+					{
+						break;
+					}					
+				}
+				else break;
+			}
+			
+			for (int i =1;i<=7;i++)
+			{
+				if(pos.row-i >=0 && pos.column-i >= 0)
+				{
+					if(gra.plansza[pos.row-i][pos.column-i].rowne(' '))
+					{
+						pMoves.add(new Pozycja(pos.row-i,pos.column-i));
+						continue;
+					}
+					if(gra.plansza[pos.row-i][pos.column-i].isLowerCase())
+					{
+						pMoves.add(new Pozycja(pos.row-i,pos.column-i));
+						break;
+					}
+					if(gra.plansza[pos.row-i][pos.column-i].isUpperCase())
+					{
+						break;
+					}					
+				}
+				else break;				
+				
+			}
+			
+			for (int i =1;i<=7;i++)
+			{
+				if(pos.row-i >= 0 && pos.column+i <= 7)
+				{
+					if(gra.plansza[pos.row-i][pos.column+i].rowne(' '))
+					{
+						pMoves.add(new Pozycja(pos.row-i,pos.column+i));
+						continue;
+					}
+					if(gra.plansza[pos.row-i][pos.column+i].isLowerCase())
+					{
+						pMoves.add(new Pozycja(pos.row-i,pos.column+i));
+						break;
+					}
+					if(gra.plansza[pos.row-i][pos.column+i].isUpperCase())
+					{
+						break;
+					}					
+				}
+				else break;					
+			}
+			
+			for (int i =1;i<=7;i++)
+			{
+				if(pos.row+i <= 7 && pos.column-i >= 0)
+				{
+					if(gra.plansza[pos.row+i][pos.column-i].rowne(' '))
+					{
+						pMoves.add(new Pozycja(pos.row+i,pos.column-i));
+						continue;
+					}
+					if(gra.plansza[pos.row+i][pos.column-i].isLowerCase())
+					{
+						pMoves.add(new Pozycja(pos.row+i,pos.column-i));
+						break;
+					}
+					if(gra.plansza[pos.row+i][pos.column-i].isUpperCase())
+					{
+						break;
+					}					
+				}
+				else break;					
+			}			
+		}	
+		
+		if(gra.plansza[pos.row][pos.column].rowne('h')) // czarna wieza
+		{
+			for (int i = pos.column-1 ; i>=0;i--) // petla do sprawdzanie ruchu w lewo
+			{
+				if(gra.plansza[pos.row][i].rowne(' ')) // i jest kolumną
+				{
+					pMoves.add(new Pozycja(pos.row,i)); 
+					continue;
+				}
+				if(gra.plansza[pos.row][i].isUpperCase())
+				{
+					pMoves.add(new Pozycja(pos.row,i));
+					break;
+				}
+				if(gra.plansza[pos.row][i].isLowerCase())
+				{
+					break;
+				}
+			}
+			for (int i = pos.column+1;i<=7;i++)	//ruch w prawo
+			{
+				if(gra.plansza[pos.row][i].rowne(' '))
+				{
+					pMoves.add(new Pozycja(pos.row,i));
+					continue;
+				}
+				if(gra.plansza[pos.row][i].isUpperCase())
+				{
+					pMoves.add(new Pozycja(pos.row,i));
+					break;
+				}
+				if(gra.plansza[pos.row][i].isLowerCase())
+				{
+					break;
+				}
+			}
+			for (int j=pos.row-1;j>=0;j--)// sprawdzanie ruchu w górę
+			{
+				if(gra.plansza[j][pos.column].rowne(' '))
+				{
+					pMoves.add(new Pozycja(j,pos.column));
+					continue;
+				}
+				if (gra.plansza[j][pos.column].isUpperCase())
+				{
+					pMoves.add(new Pozycja(j, pos.column));
+					break;
+				}
+				if (gra.plansza[j][pos.column].isLowerCase())
+				{
+					break;
+				}
+			}
+			for (int j=pos.row+1;j<=7;j++) // sprawdzanie ruchu w dol
+			{
+				if (gra.plansza[j][pos.column].rowne(' '))
+				{
+					pMoves.add(new Pozycja(j,pos.column));
+					continue;
+				}
+				if (gra.plansza[j][pos.column].isUpperCase())
+				{
+					pMoves.add(new Pozycja(j,pos.column));
+					break;
+				}
+				if(gra.plansza[j][pos.column].isLowerCase())
+				{
+					break;
+				}
+			}
+			
+			return pMoves;
+		}
+		if(gra.plansza[pos.row][pos.column].rowne('H'))
+		{
+			for (int i = pos.column -1 ; i>=0;i--) // petla do sprawdzenie ruchu w lewo
+			{
+				if(gra.plansza[pos.row][i].rowne(' '))
+				{
+					pMoves.add(new Pozycja(pos.row,i));
+					continue;
+				}
+				if(gra.plansza[pos.row][i].isLowerCase())
+				{
+					pMoves.add(new Pozycja(pos.row,i));
+					break;
+				}
+				if (gra.plansza[pos.row][i].isUpperCase())
+				{
+					break;
+				}
+			}
+			for (int i = pos.column+1;i<=7;i++)	//ruch w prawo
+			{
+				if(gra.plansza[pos.row][i].rowne(' '))
+				{
+					pMoves.add(new Pozycja(pos.row,i));
+					continue;
+				}
+				if(gra.plansza[pos.row][i].isLowerCase())
+				{
+					pMoves.add(new Pozycja(pos.row,i));
+					break;
+				}
+				if(gra.plansza[pos.row][i].isUpperCase())
+				{
+					break;
+				}
+			}
+			for (int j=pos.row-1;j>=0;j--)// sprawdzanie ruchu w górę
+			{
+				if(gra.plansza[j][pos.column].rowne(' '))
+				{
+					pMoves.add(new Pozycja(j,pos.column));
+					continue;
+				}
+				if (gra.plansza[j][pos.column].isLowerCase())
+				{
+					pMoves.add(new Pozycja(j, pos.column));
+					break;
+				}
+				if (gra.plansza[j][pos.column].isUpperCase())
+				{
+					break;
+				}
+			}
+			for (int j=pos.row+1;j<=7;j++) // sprawdzanie ruchu w dol
+			{
+				if (gra.plansza[j][pos.column].rowne(' '))
+				{
+					pMoves.add(new Pozycja(j,pos.column));
+					continue;
+				}
+				if (gra.plansza[j][pos.column].isLowerCase())
+				{
+					pMoves.add(new Pozycja(j,pos.column));
+					break;
+				}
+				if (gra.plansza[j][pos.column].isUpperCase())
+				{
+					break;
+				}
+			}
+			
+		}
+
+		
+		
+		
+		
+		return pMoves;
+	}
 	
 	public void zbij(String plansza[][], int row, int column)
 	{
