55b93eeb47681174a10cc1286723656828b16978
Konrad <Konrad@Mobilek.local>
2015-05-10 19:43:12 +0200

    lista klientow

--- ProjektSzachy/src/komunikacja/RamkaSerwera.java
+++ ProjektSzachy/src/komunikacja/RamkaSerwera.java
@@ -1,6 +1,7 @@
 package komunikacja;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class RamkaSerwera implements Serializable
 {
@@ -11,12 +12,25 @@ public class RamkaSerwera implements Serializable
 	private int rodzaj;
 	private String wiadomosc1;
 	private String wiadomosc2;
+	private ArrayList<String> klienci;
+	
 	
 	public RamkaSerwera(int r, String w1, String w2)
 	{
 		this.rodzaj=r;
 		this.wiadomosc1=w1;
 		this.wiadomosc2=w2;
+	}
+	
+	public RamkaSerwera(ArrayList<String> klienci)
+	{
+		this.rodzaj=3;
+		this.klienci = new ArrayList<String>(klienci);
+	}
+	
+	public ArrayList <String> getClientList()
+	{
+		return klienci;
 	}
 	
 	public String getW1()
